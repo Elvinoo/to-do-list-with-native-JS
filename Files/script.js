365ce6286638c5db03d7f56234f94ce796b43d3b
Elvinoo <elvinooo1990@gmail.com>
2022-11-26 14:04:00 +0400

    files uploaded

---
+++ Files/script.js
@@ -0,0 +1,11 @@
+/* const deleteList=document.querySelectorAll('.task img');
+const toDoList=document.querySelector('.to-do-list')
+console.log(toDoList.childElementCount)
+deleteList.forEach(element=>{
+    element.addEventListener('click', function(e){
+        e.target.parentElement.remove();
+        if(toDoList.childElementCount==0){
+            toDoList.style.display='none';
+        }
+    })
+}) */
