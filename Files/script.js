6a074b46842295c36cd1f0d66418e2936b8e3917
Elvinoo <elvinooo1990@gmail.com>
2022-11-26 23:46:16 +0400

    Add elements into the list done

--- Files/script.js
+++ Files/script.js
@@ -1,6 +1,28 @@
-/* const deleteList=document.querySelectorAll('.task img');
-const toDoList=document.querySelector('.to-do-list')
-console.log(toDoList.childElementCount)
+const deleteList=document.querySelectorAll('.task img');
+const toDoList=document.querySelector('.to-do-list');
+const sortIcon=document.querySelector('.sort-icon img');
+const input=document.querySelector('input')
+const inputImg=document.querySelector('.task-input img');
+const addButton=document.querySelector('.addToListButton');
+const taskList=document.querySelector('.task');
+const inputDiv=document.querySelector('.task-input');
+let i=0;
+addButton.addEventListener('click',add)
+function add(){
+    let newTask=document.createElement('div');
+    newTask.className='task';
+    i++;
+    newTask.innerHTML=
+    `<p>${i}) ${input.value}</p>
+     <img src="/files/delete-icon.svg" alt="delete">`
+    toDoList.style.display='block'
+    toDoList.appendChild(newTask);
+    input.value='';
+    if(toDoList.childElementCount==5){
+        inputDiv.style.display='none';
+    }   
+}
+
 deleteList.forEach(element=>{
     element.addEventListener('click', function(e){
         e.target.parentElement.remove();
@@ -8,4 +30,8 @@ deleteList.forEach(element=>{
             toDoList.style.display='none';
         }
     })
-}) */
+})
+inputImg.addEventListener('click', function(){
+    input.value='';
+    console.log(input.value);
+})
