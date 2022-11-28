28bf3a73668a2723b56505f3b82e02195b7d5aad
Elvinoo <elvinooo1990@gmail.com>
2022-11-28 21:32:27 +0400

    files changed...sort working only on first click;(

--- Files/script.js
+++ Files/script.js
@@ -1,13 +1,13 @@
 
 const toDoList=document.querySelector('.to-do-list');
-const sortIcon=document.querySelector('.sort-icon img');
+const sortIcon=document.querySelector('.sort-icon svg');
 const input=document.querySelector('input')
-const inputImg=document.querySelector('.task-input img');
+const inputImg=document.querySelector('.task-input svg');
 const addButton=document.querySelector('.addToListButton');
 const taskList=document.querySelector('.task');
 const inputDiv=document.querySelector('.task-input');
 
-addButton.addEventListener('click',add);
+addButton.addEventListener('click',add,true);
 input.addEventListener('keyup',function(e){
     if(e.key==='Enter'){
         add();
@@ -16,46 +16,83 @@ input.addEventListener('keyup',function(e){
 let i=toDoList.childElementCount+1;
 
 function add(){
+    if(toDoList.childElementCount<5){
     let newTask=document.createElement('div');
     newTask.className='task';
     newTask.innerHTML=
-    `<p>${i}) ${input.value}</p>
-    <img src="/files/delete-icon.svg" alt="delete">`
-    if(i==i){
-        i++
-    }
+    `<p contentEditable="true" class="tasks">${i}) ${input.value}</p>
+    <img class="gray" src="/files/delete-icon.svg" alt="cancel">
+    <img style="display: none;" class="violet" src="/files/cancel-blueviolet.svg" alt="cancel">
+`
+    i++
+   
     
     toDoList.style.display='block'
     toDoList.appendChild(newTask);
     input.value='';
     if(toDoList.childElementCount==5){
         inputDiv.style.display='none';
-    }else if(toDoList.childElementCount==0){
+    }else if(toDoList.childElementCount<5){
         inputDiv.style.display='block';
     }
+
+    const grayCancel=document.querySelectorAll('.gray');
+    const bluevioletCancel=document.querySelectorAll('.violet');
     const deleteList=document.querySelectorAll('.task img');
-deleteList.forEach(element=>{
-    element.addEventListener('click', function(e){
-        e.target.parentElement.remove();
-        i--;
-        if(toDoList.childElementCount==5){
-            inputDiv.style.display='none';
-        }else if(toDoList.childElementCount<5 && toDoList.childElementCount>0 ){
-            inputDiv.style.display='block';
-        }else if(toDoList.childElementCount==0){
-            inputDiv.style.display='block';
-            toDoList.style.display='none'; 
-        }
-        
-       /*  if(toDoList.childElementCount==0){
-            toDoList.style.display='none';
-            i==1
-        } */
-    })
-})  
+    deleteList.forEach(element=>{
+        element.addEventListener('mouseover',function(){
+            grayCancel.forEach(e=>{
+                e.style.display='none';
+            })
+            bluevioletCancel.forEach(el=>{
+                el.style.display='block';
+            })
+
+        })
+        element.addEventListener('mouseout',function(){
+            grayCancel.forEach(e=>{
+                e.style.display='inline';
+            })
+            bluevioletCancel.forEach(el=>{
+                el.style.display='none';
+            })
+        })
+        element.addEventListener('click', function(e){
+            
+            e.target.parentElement.remove();
+            if(toDoList.childElementCount==5){
+                inputDiv.style.display='none';
+            }else if(toDoList.childElementCount<5 && toDoList.childElementCount>0 ){
+                inputDiv.style.display='block';
+            }else if(toDoList.childElementCount==0){
+                inputDiv.style.display='block';
+                toDoList.style.display='none'; 
+            }
+        })
+    })  
+}
 }
 
 inputImg.addEventListener('click', function(){
     input.value='';
-    console.log(input.value);
 })
+
+sortIcon.addEventListener('click', sort);
+
+function sort(){
+    const sortIcon1=document.querySelector('.sort-icon svg:first-child');
+    const sortIcon2=document.querySelector('.sort-icon svg:last-child');
+    const taskList=document.querySelectorAll('.task');
+    let arr=[...taskList]
+    if(getComputedStyle(sortIcon1).display==='inline'){
+        sortedByIndex=[...arr].reverse();
+        toDoList.replaceChildren(...taskList, ...sortedByIndex);
+        sortIcon2.style.display='inline'
+        sortIcon1.style.display='none';
+    }else{
+        sortIcon2.style.display='none'
+        sortIcon1.style.display='block';
+        toDoList.replaceChildren(...sortedByIndex,...taskList)
+        
+    }    
+}
