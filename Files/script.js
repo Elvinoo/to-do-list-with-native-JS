6b731505ee775c6ee0610669b16cadf7eb7d1795
Elvinoo <elvinooo1990@gmail.com>
2022-11-29 21:15:27 +0400

    project ready

--- Files/script.js
+++ Files/script.js
@@ -1,34 +1,27 @@
 const toDoList = document.querySelector('.to-do-list');
-const sortIcon = document.querySelector('.sort-icon svg');
+
 const input = document.querySelector('input');
 const inputImg = document.querySelector('.task-input svg');
 const addButton = document.querySelector('.addToListButton');
 const inputDiv = document.querySelector('.task-input');
-const svg = sortIcon.innerHTML;
-const svg2 = `<svg width="25" height="15" viewBox="0 0 25 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="5" y="15" width="2.5" height="12.5" transform="rotate(-180 5 15)" fill="#C4C4C4"/>
-<rect x="10" y="3.75" width="2.5" height="7.5" transform="rotate(-90 10 3.75)" fill="#C4C4C4"/>
-<rect x="10" y="8.75" width="2.5" height="10" transform="rotate(-90 10 8.75)" fill="#C4C4C4"/>
-<rect x="10" y="13.75" width="2.5" height="15" transform="rotate(-90 10 13.75)" fill="#C4C4C4"/>
-<path d="M3.75 6.55671e-07L6.99759 4.6875L0.502404 4.6875L3.75 6.55671e-07Z" fill="#C4C4C4"/>
-</svg>`
+
 addButton.addEventListener('click', add);
 input.addEventListener('keyup', function (e) {
     if (e.key === 'Enter') {
         add();
     }
 });
-let i = toDoList.childElementCount + 1;
+
 function add() {
     if (toDoList.childElementCount < 5) {
         let newTask = document.createElement('div');
         newTask.className = 'task';
         newTask.innerHTML =
-            `<p contentEditable="true" class="tasks">${i}) ${input.value}</p>
+            `<p contentEditable="true" class="tasks"> ${input.value}</p>
     <img class="gray" src="/files/delete-icon.svg" alt="cancel">
     
 `
-        i++;
+        
         toDoList.style.display = 'block'
         toDoList.appendChild(newTask);
         input.value = '';
@@ -68,24 +61,20 @@ inputImg.addEventListener('click', function () {
     input.value = '';
 })
 
-sortIcon.addEventListener('click', sort);
+const arrowDown = document.querySelector('.sort-icon svg:first-child');
+const arrowUp= document.querySelector('.sort-icon svg:last-child')
+arrowDown.addEventListener('click', function(){
+    this.style.display='none';
+    arrowUp.style.display='inline';
+    const tasks=[...document.querySelectorAll('.task')];
+    tasks.sort((a,b)=>{return parseInt(b.innerText)-parseInt(a.innerText)});
+    toDoList.replaceChildren(...toDoList.children,...tasks)
+});
+arrowUp.addEventListener('click',function(){
+    this.style.display='none';
+    arrowDown.style.display='inline';
+    const tasks=[...document.querySelectorAll('.task')];
+    tasks.sort((a,b)=>{return parseInt(a.innerText)-parseInt(b.innerText)});
+    toDoList.replaceChildren(...toDoList.children,...tasks)
+})
 
-function sort() {
-    if (toDoList.childElementCount > 1) {
-        const taskList = document.querySelectorAll('.task');
-        let arr = [...taskList]
-        console.log(arr)
-        sortedByIndex = [...arr].reverse();
-        if (sortIcon.innerHTML == svg) {
-            toDoList.replaceChildren(...taskList, ...sortedByIndex);
-            sortIcon.innerHTML = svg2;
-
-        } else {
-            sortIcon.innerHTML = svg;
-            toDoList.replaceChildren(...sortedByIndex,...taskList);
-
-        }
-    } else {
-        alert('Nothing to sort');
-    }
-}
